fb4316760c0ad154ff32cb711e76a62b8cb9d987
Mightysmallfry <90732393+Mightysmallfry@users.noreply.github.com>
2025-10-29 19:15:04 -0700

    Update to the project

---
+++ Cameras/SubpixelCamera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+
+var actual_cam_pos : Vector2
+var subpixel_offset : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	actual_cam_pos = actual_cam_pos.lerp($"../Player".global_position, delta * 3.0)
+	
+	subpixel_offset = actual_cam_pos.round() - actual_cam_pos
+	
+	Globals.GameViewportContainer.material.set_shader_parameter("cam_offset", subpixel_offset)
+	
+	global_position = actual_cam_pos.round()
